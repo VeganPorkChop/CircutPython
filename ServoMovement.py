2597f4df0434d4e476f0e1b868639a39467cd158
VeganPorkChop <ggilber72@charlottesvilleschools.org>
2022-09-12 15:57:10 -0400

    zetzzserzsefzsdf

--- ServoMovement.py
+++ ServoMovement.py
@@ -1,14 +1,23 @@
-import servo
-import math
-import time
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
 
-myServo = 3
-servoMove = 1000
-servoSecondMove = 50000
+"""CircuitPython Essentials Servo standard servo example"""
+import time
+import board
+import pwmio
+from adafruit_motor import servo
+
+# create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.D3, duty_cycle=2 ** 15, frequency=50)
+
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
 
 while True:
-    myServo.angle = 1090
-    time.sleep(1)
-    myServo.angle = 50000
-    time.sleep(1)
-    print("qowid")
+    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
+    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
