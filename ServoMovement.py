667e21e542dda19134b5d10b6f6bd5ae1a7c0d7b
VeganPorkChop <ggilber72@charlottesvilleschools.org>
2022-09-13 15:23:22 -0400

    game addition (sorta

--- ServoMovement.py
+++ ServoMovement.py
@@ -4,19 +4,26 @@
 
 """CircuitPython Essentials Servo standard servo example"""
 import time
+import neopixel
 import board
 import pwmio
 from adafruit_motor import servo
 # create a PWMOut object on Pin A2.
 pwm = pwmio.PWMOut(board.D3, duty_cycle=2 ** 15, frequency=50)
-
+Kaz = neopixel.NeoPixel(board.NEOPIXEL, 1)
+BUTTON = 1
+Kaz.brightness = 1
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
 
 while True:
-    for angle in range(0, 180, 100):  # 0 - 180 degrees, 100 degrees at a time.
+ if BUTTON == 
+    for angle in range(0, 180, 180):  # 0 - 180 degrees, 100 degrees at a time.
         my_servo.angle = angle
-        time.sleep(2)
-    for angle in range(180, 0, -100): # 180 - 0 degrees, 100 degrees at a time.
+    for angle in range(180, 0, -180): # 180 - 0 degrees, 100 degrees at a time.
         my_servo.angle = angle
-        time.sleep(2)
+        Kaz.fill((0, 255, 0))
+        time.sleep(.5)
+        Kaz.fill((255, 0, 0))
+        time.sleep(1.5)
+        
--- code.py
+++ code.py
@@ -2,11 +2,11 @@ import board
 import neopixel
 import time
 
-Kaz = neopixel.NeoPixel(board.NEOPIXEL, 1)
-Kaz.brightness = 1  
+Kaz = neopixel.NeoPixel(board.NEOPIXEL, 1)#connecting the neopixel on the board to the code
+Kaz.brightness = 1  #setting the brightness of the light, from 0-1 brightness
 
 while True:
-    Kaz.fill((204, 0, 153))
-    time.sleep(3)
+    Kaz.fill((204, 0, 153))#setting the color with RGB values
+    time.sleep(3)#pausing code
     Kaz.fill((204, 255, 204))
     time.sleep(3)
