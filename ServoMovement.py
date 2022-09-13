99e249ceaf4c94d0af6aac48f5e42d3f46172f53
VeganPorkChop <ggilber72@charlottesvilleschools.org>
2022-09-13 14:52:53 -0400

    finihsed servo code

--- ServoMovement.py
+++ ServoMovement.py
@@ -14,9 +14,9 @@ pwm = pwmio.PWMOut(board.D3, duty_cycle=2 ** 15, frequency=50)
 my_servo = servo.Servo(pwm)
 
 while True:
-    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+    for angle in range(0, 180, 100):  # 0 - 180 degrees, 100 degrees at a time.
         my_servo.angle = angle
-        time.sleep(0.05)
-    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+        time.sleep(2)
+    for angle in range(180, 0, -100): # 180 - 0 degrees, 100 degrees at a time.
         my_servo.angle = angle
-        time.sleep(0.05)
+        time.sleep(2)
