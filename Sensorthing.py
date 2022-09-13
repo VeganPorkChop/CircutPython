34990cefdd2347c6730ee2f56f151a33b19e4584
VeganPorkChop <ggilber72@charlottesvilleschools.org>
2022-09-13 16:01:25 -0400

    end of 9/13/22

---
+++ Sensorthing.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import adafruit_hcsr04
+import neopixel
+
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
+Kaz = neopixel.NeoPixel(board.NEOPIXEL, 1)#connecting the neopixel on the board to the code
+Kaz.brightness = 1  #setting the brightness of the light, from 0-1 brightness
+
+while True:
+    try:
+        cm = sonar.distance
+        print((sonar.distance,"herpderp"))
+        time.sleep(0.5)
+        if cm > 5:
+            Kaz.fill((0, 0, 255))#setting the color with RGB values
+        elif cm > 5 and cm < 20:
+            Kaz.fill((255, 0, 0))#setting the color with RGB values
+        else:
+            Kaz.fill((0, 255, 0))#setting the color with RGB values
+    except RuntimeError:
+        print("Retrying!")
+    time.sleep(0.1)
