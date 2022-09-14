41f8a8a3dca446512ef454b94a991ed0835aa320
VeganPorkChop <ggilber72@charlottesvilleschools.org>
2022-09-14 11:05:32 -0400

    drfg

--- Sensorthing.py
+++ Sensorthing.py
@@ -8,16 +8,16 @@ import neopixel
 
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
 Kaz = neopixel.NeoPixel(board.NEOPIXEL, 1)#connecting the neopixel on the board to the code
-Kaz.brightness = 1  #setting the brightness of the light, from 0-1 brightness
+Kaz.brightness = .3  #setting the brightness of the light, from 0-1 brightness
 
 while True:
     try:
         cm = sonar.distance
-        print((sonar.distance,"herpderp"))
+        print((sonar.distance))
         time.sleep(0.5)
         if cm > 5:
             Kaz.fill((0, 0, 255))#setting the color with RGB values
-        elif cm > 5 and cm < 20:
+        if cm > 5 and cm < 20:
             Kaz.fill((255, 0, 0))#setting the color with RGB values
         else:
             Kaz.fill((0, 255, 0))#setting the color with RGB values
