5558cac8cd17a3b28ad283452da85cbc23570bce
VeganPorkChop <ggilber72@charlottesvilleschools.org>
2022-09-23 13:17:15 -0400

    Finished LCD code, all done lol

--- LCD.py
+++ LCD.py
@@ -1,4 +1,5 @@
 import board
+import time
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
 from digitalio import DigitalInOut, Direction, Pull
@@ -15,20 +16,34 @@ btn2.pull = Pull.UP
 lcd = LCD(I2CPCF8574Interface(i2c, 0x3f), num_rows=2, num_cols=16)
 cur_state = True
 prev_state = True
+cur_state2 = True
+prev_state2 = True
 buttonPress = 0
 
 while True:
-    if btn2 == True:
-        lcd.print("BTN is down")
+    while btn2.value == False:
         cur_state = btn.value
         if cur_state != prev_state:
             if not cur_state:
                 buttonPress = buttonPress + 1
                 lcd.clear()
-                lcd.set_cursor_pos(1, 0)
-                lcd.print("BTN Press:")
+                lcd.set_cursor_pos(0,0)
+                lcd.print(str(buttonPress))
+            else:
+                lcd.clear()
+                lcd.set_cursor_pos(0,0)
                 lcd.print(str(buttonPress))
         prev_state = cur_state
     else:
-        lcd.set_cursor_pos(0, 0)
-        lcd.print("BTN is up")
+        cur_state2 = btn.value
+        if cur_state2 != prev_state2:
+            if not cur_state2:
+                buttonPress = buttonPress - 1
+                lcd.clear()
+                lcd.set_cursor_pos(0,0)
+                lcd.print(str(buttonPress))
+            else:
+                lcd.clear()
+                lcd.set_cursor_pos(0,0)
+                lcd.print(str(buttonPress))
+        prev_state2 = cur_state2
