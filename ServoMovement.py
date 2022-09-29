e8048618c839cc46d320439a1b88445b6cfddd72
VeganPorkChop <ggilber72@charlottesvilleschools.org>
2022-09-29 15:10:03 -0400

    xzcv

--- ServoMovement.py
+++ ServoMovement.py
@@ -3,21 +3,30 @@
 # SPDX-License-Identifier: MIT
 
 """CircuitPython Essentials Servo standard servo example"""
+from digitalio import DigitalInOut, Direction, Pull
 import time
 import neopixel
 import board
 import pwmio
 from adafruit_motor import servo
+btn = DigitalInOut(board.D3)
+btn2 = DigitalInOut(board.D2)
+btn.direction = Direction.INPUT
+btn.pull = Pull.UP
+btn2.direction = Direction.INPUT
+btn2.pull = Pull.UP
 # create a PWMOut object on Pin A2.
 pwm = pwmio.PWMOut(board.D3, duty_cycle=2 ** 15, frequency=50)
-Kaz = neopixel.NeoPixel(board.NEOPIXEL, 1)
-BUTTON = 1
-Kaz.brightness = 1
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
 
 while True:
-    for angle in range(0, 180, 180):  # 0 - 180 degrees, 100 degrees at a time.
-        my_servo.angle = angle
-    for angle in range(180, 0, -180): # 180 - 0 degrees, 100 degrees at a time.
-        my_servo.angle = angle
+    time.sleep(.1)
+    if btn == True:
+        for angle in range(0, 180, 90):  # 0 - 180 degrees, 100 degrees at a time.
+            my_servo.angle = angle
+    elif btn2 == True:
+        for angle in range(180, 0, -90): # 180 - 0 degrees, 100 degrees at a time.
+            my_servo.angle = angle
+    else:
+        print("click a button man")
